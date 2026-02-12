44bf33de12758d96f6fb63c52fb558c5aa5eab8f
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-02-12 22:58:26 +0700

    carousel component

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
