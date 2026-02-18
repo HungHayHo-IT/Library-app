e60735bd3ae76349109efef73991498a0e6a37dd
HungHayHo <nguyenphuocnhahungl97@gmail.com>
2026-02-18 20:47:42 +0700

    backend

--- backend/src/main/java/com/example/Library_app/dao/ReviewRepository.java
+++ backend/src/main/java/com/example/Library_app/dao/ReviewRepository.java
@@ -1,4 +1,15 @@
 package com.example.Library_app.dao;
 
-public interface ReviewRepository {
+import com.example.Library_app.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// CHÚ Ý DÒNG NÀY:
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    // Hàm này sẽ hết lỗi
+    Page<Review> findByBookId(@RequestParam("book_id") Long bookId, Pageable pageable);
 }
